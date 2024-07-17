220ae121623223fb0676ad4ddc83dd562147b1a5
canlion <canlion@naver.com>
2024-07-18 01:42:10 +0900

    feat: 회원 엔티티 Member 추가

---
+++ api/src/main/java/burgermap/members/Member.java
@@ -0,0 +1,15 @@
+package burgermap.members;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor  // ObjectMapper 적용 목적
+@AllArgsConstructor  // 단위 테스트시 더미 데이터 생성 목적
+public class Member {
+    private Long memberId;
+    private String loginId;
+    private String password;
+    private String email;
+}
