a56d6bc712b795a31c4a9753242b314d76078ad7
canlion <canlion@naver.com>
2024-07-23 00:45:29 +0900

    refactor: 파일 이름 변경
    - MemberController 테스트 코드임을 명확히 알 수 있도록 이름 변경

--- api/src/test/java/burgermap/controller/MemberControllerTest.java
+++ api/src/test/java/burgermap/controller/MemberControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class MainTest {
+class MemberControllerTest {
 
     private ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
