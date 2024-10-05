eab8c75f5e1fd58fef30d86c8e066f4df24e0f2c
canlion <canlion@naver.com>
2024-10-05 19:46:20 +0900

    feat: SpringDataJPA/MySQL 회원 레포지터리 적용
    - 회원 엔티티 관리 방식을 HashMap에서 SpringDataJPA/MySQL로 변경

--- api/src/main/java/burgermap/service/MemberService.java
+++ api/src/main/java/burgermap/service/MemberService.java
@@ -7,11 +7,13 @@ import burgermap.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
