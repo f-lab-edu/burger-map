c5000a36d04ef6479f9378eeadf42a860ac1f77a
canlion <canlion@naver.com>
2024-10-05 14:57:00 +0900

    feat: 테스트용 MySQL 컨테이너 설정 클래스 추가
    - Testcontainers MySQL 컨테이너 환경 구성
    - 레포지터리 테스트 클래스에서 상속하여 사용

---
+++ api/src/test/java/burgermap/repository/TestcontainersTest.java
@@ -0,0 +1,40 @@
+package burgermap.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@Transactional
+class TestcontainersTest {
+    @Container
+    private static MySQLContainer container = new MySQLContainer("mysql:8.3.0")
+            .withDatabaseName("burgermap-test")
+            .withUsername("user")
+            .withPassword("useruser");
+
+    @DynamicPropertySource  // 동적으로 프로퍼티 설정
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+        registry.add("spring.jpa.properties.hibernate.hbm2ddl.import_files", () -> "init.sql");
+        registry.add("logging.level.org.hibernate.SQL", () -> "DEBUG");
+        registry.add("logging.level.org.hibernate.orm.dbc.bind", () -> "TRACE");
+    }
+
+    @Test
+    @DisplayName("MySQL 컨테이너 동작")
+    void containerTest() {
+        Assertions.assertThat(container.isRunning()).isTrue();
+    }
+}
