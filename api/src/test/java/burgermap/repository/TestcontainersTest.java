9db5c286c2d514a3167e28f2aeef8d643050f547
canlion <canlion@naver.com>
2024-10-06 18:19:08 +0900

    fix: MySQL 컨테이너 객체를 인스턴스 필드로 변경
    - 컨테이너를 static으로 선언하는 경우 여러 테스트 클래스에서 상속하여
      사용할 때 컨테이너가 동작하지 않음.
      - 원인 파악 후 재시도 예정

--- api/src/test/java/burgermap/repository/TestcontainersTest.java
+++ api/src/test/java/burgermap/repository/TestcontainersTest.java
@@ -15,22 +15,28 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @Transactional
 class TestcontainersTest {
+//    @Container
+//    private static MySQLContainer container = new MySQLContainer("mysql:8.3.0")
+//            .withDatabaseName("burgermap-test")
+//            .withUsername("user")
+//            .withPassword("useruser");
+
     @Container
-    private static MySQLContainer container = new MySQLContainer("mysql:8.3.0")
+    private MySQLContainer container = new MySQLContainer("mysql:8.3.0")
             .withDatabaseName("burgermap-test")
             .withUsername("user")
             .withPassword("useruser");
 
-    @DynamicPropertySource  // 동적으로 프로퍼티 설정
-    static void dynamicProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", container::getJdbcUrl);
-        registry.add("spring.datasource.username", container::getUsername);
-        registry.add("spring.datasource.password", container::getPassword);
-        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
-        registry.add("spring.jpa.properties.hibernate.hbm2ddl.import_files", () -> "init.sql");
-        registry.add("logging.level.org.hibernate.SQL", () -> "DEBUG");
-        registry.add("logging.level.org.hibernate.orm.jdbc.bind", () -> "TRACE");
-    }
+//    @DynamicPropertySource  // 동적으로 프로퍼티 설정
+//    static void dynamicProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.datasource.url", container::getJdbcUrl);
+//        registry.add("spring.datasource.username", container::getUsername);
+//        registry.add("spring.datasource.password", container::getPassword);
+//        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+//        registry.add("spring.jpa.properties.hibernate.hbm2ddl.import_files", () -> "init.sql");
+//        registry.add("logging.level.org.hibernate.SQL", () -> "DEBUG");
+//        registry.add("logging.level.org.hibernate.orm.jdbc.bind", () -> "TRACE");
+//    }
 
     @Test
     @DisplayName("MySQL 컨테이너 동작")
