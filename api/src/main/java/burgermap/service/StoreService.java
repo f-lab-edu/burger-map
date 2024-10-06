88bfb51d3c97ad5c0a2cf150d8c1feaa4592acb1
canlion <canlion@naver.com>
2024-10-06 20:56:59 +0900

    fix: 가게 서비스 레이어 @Transactional 적용

--- api/src/main/java/burgermap/service/StoreService.java
+++ api/src/main/java/burgermap/service/StoreService.java
@@ -8,12 +8,14 @@ import burgermap.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class StoreService {
     private final MemberService memberService;
