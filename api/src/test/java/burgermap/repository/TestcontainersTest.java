14638f8185f7efd81793c07d6971f9fe3aca19ea
canlion <canlion@naver.com>
2024-10-09 15:40:16 +0900

    style: 불필요한 주석 제거

--- api/src/test/java/burgermap/repository/TestcontainersTest.java
+++ api/src/test/java/burgermap/repository/TestcontainersTest.java
@@ -4,8 +4,6 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -15,28 +13,11 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 @Testcontainers
 @Transactional
 class TestcontainersTest {
-//    @Container
-//    private static MySQLContainer container = new MySQLContainer("mysql:8.3.0")
-//            .withDatabaseName("burgermap-test")
-//            .withUsername("user")
-//            .withPassword("useruser");
-
     @Container
     private MySQLContainer container = new MySQLContainer("mysql:8.3.0")
             .withDatabaseName("burgermap-test")
             .withUsername("user")
             .withPassword("useruser");
-
-//    @DynamicPropertySource  // 동적으로 프로퍼티 설정
-//    static void dynamicProperties(DynamicPropertyRegistry registry) {
-//        registry.add("spring.datasource.url", container::getJdbcUrl);
-//        registry.add("spring.datasource.username", container::getUsername);
-//        registry.add("spring.datasource.password", container::getPassword);
-//        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
-//        registry.add("spring.jpa.properties.hibernate.hbm2ddl.import_files", () -> "init.sql");
-//        registry.add("logging.level.org.hibernate.SQL", () -> "DEBUG");
-//        registry.add("logging.level.org.hibernate.orm.jdbc.bind", () -> "TRACE");
-//    }
 
     @Test
     @DisplayName("MySQL 컨테이너 동작")
