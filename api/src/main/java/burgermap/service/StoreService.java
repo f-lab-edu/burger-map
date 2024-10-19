dca5d58fedea6fffafb23c09b386791e950db17e
canlion <canlion@naver.com>
2024-10-19 17:44:27 +0900

    style: 코드 스타일 통일을 위해 줄바꿈 적용

--- api/src/main/java/burgermap/service/StoreService.java
+++ api/src/main/java/burgermap/service/StoreService.java
@@ -33,7 +33,8 @@ public class StoreService {
     }
 
     public Store getStore(Long storeId) {
-        Store store = storeRepository.findByStoreId(storeId).orElseThrow(() -> new StoreNotExistException(storeId));
+        Store store = storeRepository.findByStoreId(storeId)
+                .orElseThrow(() -> new StoreNotExistException(storeId));
         log.debug("store info: {}", store);
         return store;
     }
