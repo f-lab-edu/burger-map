65f503b05536b286b18ad83907059dbd6a38f594
canlion <canlion@naver.com>
2024-11-08 00:52:25 +0900

    feat: 리뷰 서비스 추가
    - 리뷰 추가, 제거, 조회 기능 제공

---
+++ api/src/main/java/burgermap/service/ReviewService.java
@@ -0,0 +1,59 @@
+package burgermap.service;
+
+import burgermap.entity.Food;
+import burgermap.entity.Review;
+import burgermap.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository repository;
+
+    private final ReviewLookupService reviewLookupService;
+    private final MemberLookupService memberLookupService;
+    private final StoreLookupService storeLookupService;
+    private final FoodLookupService foodLookupService;
+
+    public void addReview(Review review, Long memberId, Long foodId) {
+        // TODO: 회원이 CUSTOMER 타입인지 확인, 아닌 경우 예외
+        Food food = foodLookupService.findByFoodId(foodId);
+        review.setFood(food);
+        review.setStore(food.getStore());
+        review.setMember(memberLookupService.findByMemberId(memberId));
+        repository.save(review);
+        log.debug("review added: {}", review);
+    }
+
+    public Review getReview(Long reviewId) {
+        return reviewLookupService.findByReviewId(reviewId);
+    }
+
+    public Review deleteReview(Long reviewId, Long memberId) {
+        Long requestMemberId = memberLookupService.findByMemberId(memberId).getMemberId();
+        Review review = reviewLookupService.findByReviewId(reviewId);
+
+        // TODO: 리뷰 작성 회원과 삭제 요청 회원이 같은지 확인, 아닌 경우 예외
+        repository.deleteByReviewId(reviewId);
+        log.debug("review deleted: {}", review);
+        return review;
+    }
+
+    public List<Review> getStoreReviews(Long storeId) {
+        return repository.findByStoreId(storeId);
+    }
+
+    public List<Review> getMemberReviews(Long memberId) {
+        return repository.findByMemberId(memberId);
+    }
+
+    public List<Review> getFoodReviews(Long foodId) {
+        return repository.findByFoodId(foodId);
+    }
+}
